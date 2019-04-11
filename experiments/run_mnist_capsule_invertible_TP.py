d3a73eb1e8ea78ba9970a35cd5ac41f90551fbdb
alexander <alexander.meulemans@gmail.com>
2019-04-11 14:41:55 +0200

    path adjusted

--- experiments/run_mnist_capsule_invertible_TP.py
+++ experiments/run_mnist_capsule_invertible_TP.py
@@ -7,6 +7,9 @@ You may obtain a copy of the License at
 
    http://www.apache.org/licenses/LICENSE-2.0
 """
+
+import sys
+sys.path.append('.')
 from layers.invertible_layer import InvertibleLeakyReluLayer
 from layers.invertible_layer import InvertibleInputLayer
 from layers.invertible_layer import InvertibleCapsuleOutputLayer
